46f79f9e2411c74eb6f7a4636c72f09f99a5a0bc
ahsan.awan <ahsan.awan@sofodynamix.com>
2023-09-06 02:59:16 +0500

    final

--- lib/src/ui/views/listDetails.dart
+++ lib/src/ui/views/listDetails.dart
@@ -1,3 +1,4 @@
+import 'package:docapp/src/utils/config/uidata.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -10,17 +11,183 @@ class ListsDetailPage extends StatelessWidget {
   // final String docType = Get.arguments['docType'];
 
   Widget bodyData() {
-
     return GetBuilder<ListDetailController>(
       builder: (_) {
         return SafeArea(
+          minimum: EdgeInsets.all(20),
           child: _.isLoading
               ? const Center(
                   child: CircularProgressIndicator(),
                 )
               : SingleChildScrollView(
-                  child: Column(
-                    children: [Text("OWNER: ${_.allPro["owner"]}")],
+                  child: Container(
+                    height: Get.height / 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Proj Name:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["project_name"]}',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Exp Start Date:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["expected_start_date"]}", ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Exp End Date:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["expected_end_date"]}, ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Priority:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["priority"]}, ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Department:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["department"]}, ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Owner:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["owner"]}, ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Status:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["status"]} ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Day to send:  ',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: '${_.allPro["day_to_send"]} ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    color: UIDataColors.DarkGreenColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
         );
